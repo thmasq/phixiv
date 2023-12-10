4fc424a748e995a3eda7b4828c7cda99f5a47097
thelaao <104701180+thelaao@users.noreply.github.com>
2023-12-10 11:38:20 -0300

    read pximg base url from environment variable
    dont send an Authorization header when fetching images as it is unnecessary

--- src/proxy.rs
+++ src/proxy.rs
@@ -1,4 +1,4 @@
-use std::{sync::Arc, time::Duration};
+use std::{sync::Arc, time::Duration, env};
 
 use axum::{
     body::StreamBody,
@@ -22,14 +22,11 @@ async fn proxy_handler(
 ) -> Result<impl IntoResponse, PhixivError> {
     let state = state.read().await;
 
-    let url = format!("https://i.pximg.net/{path}");
+    let base = env::var("PXIMG_BASE").unwrap_or_else(|_| String::from("https://i.pximg.net/"));
+    let url = format!("{base}{path}");
 
     let mut headers = helper::headers();
     headers.append("Referer", "https://www.pixiv.net/".parse()?);
-    headers.append(
-        "Authorization",
-        format!("Bearer {}", state.auth.access_token).parse()?,
-    );
 
     let response = state.client.get(&url).headers(headers).send().await?;
 
